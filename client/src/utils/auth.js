74a0ac76576b26f5d4b8485c5d0194dceef027af
Finn Stitcher <foundfootage@protonmail.com>
2022-09-30 15:21:10 -0700

    login

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -29,21 +29,21 @@ class AuthService {
 
   getToken() {
     // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
+    return localStorage.getItem('ll_id_token');
   }
 
   login(idToken) {
     // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken);
+    localStorage.setItem('ll_id_token', idToken);
     window.location.assign('/');
   }
 
   logout() {
     // Clear user token and profile data from localStorage
-    localStorage.removeItem('id_token');
+    localStorage.removeItem('ll_id_token');
     // this will reload the page and reset the state of the application
     window.location.assign('/');
   }
-}
+};
 
 export default new AuthService();
